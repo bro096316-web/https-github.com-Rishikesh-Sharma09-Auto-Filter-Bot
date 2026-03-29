ce1cfd3d07dd67c309eafb0f84c3c55b3d9efa03
bro096316-web <bro096316@gmail.com>
2026-03-29 17:30:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -73,8 +73,8 @@ async def start(client, message):
                 ],[
                     InlineKeyboardButton('💰 ᴇᴀʀɴ ᴍᴏɴᴇʏ ʙʏ ʙᴏᴛ 💰', callback_data='earn')
                 ],[
-InlineKeyboardButton(' ⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ ⌬; callback_data='group')
-InlineKeyboardButton(' ✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇; callback_data='channel')
+InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ ⌬', callback_data='group')
+InlineKeyboardButton('✇ Jᴏɪɴ REQUEST Channel', callback_data='channel')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
